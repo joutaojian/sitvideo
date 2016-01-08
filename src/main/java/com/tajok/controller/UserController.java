dbaac8693f5e585e9322252a025202ddc4ce831b
tajok <jou_taojian@163.com>
2016-01-08 15:12:06 +0800

    update UserController

--- src/main/java/com/tajok/controller/UserController.java
+++ src/main/java/com/tajok/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.tajok.model.User;
 import com.tajok.service.IUserService;
@@ -13,13 +14,29 @@ import com.tajok.service.IUserService;
 public class UserController {
 
 	@Resource
-	private IUserService userService;
+	private IUserService userService;//将本地的userService属性注入，等同且替代了setter
 
+	/*
 	@RequestMapping("/showUser")
-	public String toIndex(HttpServletRequest request, Model model) {
+	public String showUser(HttpServletRequest request, Model model) {
 		int userId = Integer.parseInt(request.getParameter("id"));
 		User user = this.userService.getUserById(userId);
 		model.addAttribute("user", user);
 		return "showUser";
+	}
+	*/
+	
+	@RequestMapping("/showUser/{id}")
+	public String showUser(HttpServletRequest request,@PathVariable int id) {//@PathVariable用来动态传参，url更美观
+		User user = userService.getUserById(id);
+		request.setAttribute("user", user);
+		return "showUser";
+	}
+	
+	@RequestMapping("/auditUser/{id}")
+	public String auditUser(HttpServletRequest request,@PathVariable int id) {
+		User user = userService.getUserById(id);
+		request.setAttribute("user", user);
+		return "auditUser";
 	}
 }
