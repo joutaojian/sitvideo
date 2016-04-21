c6be6d7c64abb11fcf0398e28e1f7c0496c79576
tajok <jou_taojian@.163.com>
2016-04-21 14:30:03 +0800

    update database

--- mybatis.sql
+++ mybatis.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50629
 File Encoding         : 65001
 
-Date: 2016-03-14 18:01:38
+Date: 2016-04-21 14:29:03
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -89,7 +89,7 @@ DROP TABLE IF EXISTS `t_mov`;
 CREATE TABLE `t_mov` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `desc` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `descp` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `mov_url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `img_url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
@@ -101,11 +101,13 @@ CREATE TABLE `t_mov` (
   KEY `mov_user` (`user_id`),
   CONSTRAINT `mov_level` FOREIGN KEY (`level_id`) REFERENCES `t_level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `mov_user` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of t_mov
 -- ----------------------------
+INSERT INTO `t_mov` VALUES ('14', 'qwewqw', 'www', '2016-04-14 09:27:19', null, 'upload/201604140927190638pic20.jpg', 'upload/201604140927191882test.mp4', '1', '6');
+INSERT INTO `t_mov` VALUES ('15', '123', '1', '2016-04-14 09:27:44', null, 'upload/201604140927432732pic20.jpg', 'upload/201604140927439400test.mp4', '1', '6');
 
 -- ----------------------------
 -- Table structure for `t_record`
@@ -142,11 +144,23 @@ CREATE TABLE `t_user` (
   PRIMARY KEY (`id`),
   KEY `user_level` (`level_id`),
   CONSTRAINT `user_level` FOREIGN KEY (`level_id`) REFERENCES `t_level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
 INSERT INTO `t_user` VALUES ('1', '周韬健', 'admin', 'admin@qq.com', '2147483647', '2016-03-10 17:07:12', '4');
-INSERT INTO `t_user` VALUES ('2', '张三', null, '123456@qq.com', '2147483647', '2016-03-09 18:23:01', '2');
 INSERT INTO `t_user` VALUES ('3', '李四', null, '123456789@163.com', '1324567897', '2016-03-09 19:07:28', '2');
+INSERT INTO `t_user` VALUES ('5', 'adminaas', '1', 'admin@qqa.com', '1237', null, null);
+INSERT INTO `t_user` VALUES ('6', 'test', '123456', 'qrb@163.com', '1380013800', '2016-03-16 15:46:42', null);
+INSERT INTO `t_user` VALUES ('7', 'adminaas', '1', '740146442@qq.comdd', '1237', null, '1');
+INSERT INTO `t_user` VALUES ('8', '?XX', 'zhou', 'aa@qqq.com', '1357809424', '2016-03-09 19:07:28', '4');
+INSERT INTO `t_user` VALUES ('9', '?XX', 'zhou', 'aa@qqq.com', '1357809424', '2016-03-09 19:07:28', '4');
+INSERT INTO `t_user` VALUES ('10', '?XX', 'zhou', 'aa@qqq.com', '1357809424', '2016-03-09 19:07:28', '4');
+INSERT INTO `t_user` VALUES ('11', 'psName', 'psPassword', 'jkjs@126.com', null, null, null);
+INSERT INTO `t_user` VALUES ('12', 'psName', 'psPassword', 'jkjs@126.com', null, null, null);
+INSERT INTO `t_user` VALUES ('13', 'lucy', '123', 'jkjs@126.com', null, '2016-04-01 16:29:00', null);
+INSERT INTO `t_user` VALUES ('14', 'psName', 'psPassword', 'jkjs@126.com', null, null, null);
+INSERT INTO `t_user` VALUES ('15', 'psName', 'psPassword', 'jkjs@126.com', null, null, null);
+INSERT INTO `t_user` VALUES ('16', 'psName', 'psPassword', 'jkjs@126.com', null, null, null);
+INSERT INTO `t_user` VALUES ('17', 'psName', 'psPassword', 'jkjs@126.com', null, null, null);
